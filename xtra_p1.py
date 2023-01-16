e4d0daac1ada7dc71d948cd9b7db0d682bd78873
krh5284 <krh5284@gmail.com>
2023-01-16 12:47:08 -0500

    Task 6

--- xtra_p1.py
+++ xtra_p1.py
@@ -7,12 +7,12 @@ import random
 
 # Change the name below to a name of your choice
 
-name = "GameBot"
+name = "Billy"
 
 # Fix the code below to print the name using an f-string
 
 print()
-print("Hello, I'm {name}, your gamebot.")
+print(f"Hello, I'm {name}, your gamebot.")
 print("Let's play an animal guessing game!")
 print("There are 3 animals: wolf, eagle, snake (a Python of course).")
 print("The wolf scares the eagle.")
@@ -26,7 +26,7 @@ print()
 # enter wolf, eagle, or snake.
 # Hint: use the input() function
 
-user_choice = "wolf"
+user_choice = input('Choose "wolf", "eagle", or "snake":  ')
 
 # Now the bot will pick one
 buddy_choice = random.choice(["wolf", "eagle", "snake"])
@@ -44,11 +44,23 @@ print()
 # In Python, indentation is important!
 if user_choice == buddy_choice:
     print("We tied!")
+if (user_choice == "wolf") and (buddy_choice == "snake"):
+    print("You win")
+if (user_choice == "wolf") and (buddy_choice == "eagle"):
+    print("I win")
+if (user_choice == "eagle") and (buddy_choice == "snake"):
+    print("You win")
+if (user_choice == "eagle") and (buddy_choice == "wolf"):
+    print("I win")
+if (user_choice == "snake") and (buddy_choice == "wolf"):
+    print("You win")
+if (user_choice == "snake") and (buddy_choice == "eagle"):
+    print("I win")
 
 
 # When you finish,
 # right-click on the code and select "Format Document"
 
 # Run the code, and play the game a few times.
-# Copy the output from the terminal and paste it into 
+# Copy the output from the terminal and paste it into
 # a new file named xtra_p1out.txt.
